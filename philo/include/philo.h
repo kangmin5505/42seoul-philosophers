ca29814105ba46b0c052774d741d504e0c27f5d5
kangmin <kangmin5505@naver.com>
2022-03-01 16:27:55 +0900

    Done parse input

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,8 +6,38 @@
 /*   By: kangkim <kangkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 14:24:39 by kangkim           #+#    #+#             */
-/*   Updated: 2022/02/28 14:24:45 by kangkim          ###   ########.fr       */
+/*   Updated: 2022/03/01 16:26:49 by kangkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PHILO_H
+# define PHILO_H
 
+# include <stddef.h>
+# include <unistd.h>
+# include <stdbool.h>
+
+typedef struct s_main_args	t_main_args;
+
+struct s_main_args
+{
+	size_t	n_philos;
+	size_t	time_die;
+	size_t	time_eat;
+	size_t	time_sleep;
+	size_t	n_must_eat;
+	bool	optional_arg;
+};
+
+enum e_exit_status
+{
+	SUCCESS,
+	PARSE_FAIL,
+	ALLOC_FAIL,
+	MUTEX_FAIL,
+	RUNTIME_FAIL
+};
+
+size_t			ft_strlen(const char *str);
+bool			parse_input(int argc, const char **argv, t_main_args *main_args);
+#endif
--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,8 +6,37 @@
 /*   By: kangkim <kangkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 14:27:20 by kangkim           #+#    #+#             */
-/*   Updated: 2022/02/28 14:27:21 by kangkim          ###   ########.fr       */
+/*   Updated: 2022/03/01 16:26:35 by kangkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "philo.h"
 
+static enum e_exit_status	print_msg(enum e_exit_status exit_status)
+{
+	size_t	len;
+	static const char	*msg[] = {
+		"",
+		"Usage: ./philo n_phios time_die time_eat time_sleep [n_must_eat]\n",
+		"Error: Allocation fail\n",
+		"Error: Mutex fail\n",
+		"Error: Runtime fail\n"
+	};
+
+	if (exit_status == SUCCESS)
+		return (SUCCESS);
+	len = ft_strlen(msg[exit_status]);
+	write(STDERR_FILENO, msg[exit_status], len);
+	return (exit_status);
+}
+
+int	main(int argc, const char **argv)
+{
+	int			result;
+	t_main_args	main_args;
+
+	result = SUCCESS;
+	if (parse_input(argc, argv, &main_args) == false)
+		return (print_msg(PARSE_FAIL));
+	return (print_msg(result));
+}
